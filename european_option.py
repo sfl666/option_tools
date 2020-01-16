e8bf315bbca42916482eccdbecf70e4734109b6b
shifulin <s.flin@foxmail.com>
2020-01-16 23:43:24 +0800

    历史的隐含波动率

---
+++ european_option.py
@@ -0,0 +1,129 @@
+"""
+Author: shifulin
+Email: shifulin666@qq.com
+"""
+from math import log, sqrt, exp
+# import numpy as np
+from scipy.stats import norm
+
+
+# def bs_call(s, k, sigma, r, t):
+#     d1 = (np.log(s / k) + (r + pow(sigma, 2) / 2.0) * t) / (sigma * np.sqrt(t))
+#     d2 = d1 - sigma * np.sqrt(t)
+#     return s * norm.cdf(d1) - k * np.exp(-r * t) * norm.cdf(d2)
+#
+#
+# def bs_put(s, k, sigma, r, t):
+#     d1 = (np.log(s / k) + (r + pow(sigma, 2) / 2.0) * t) / (sigma * np.sqrt(t))
+#     d2 = d1 - sigma * np.sqrt(t)
+#     return k * np.exp(-r * t) * norm.cdf(-d2) - s * norm.cdf(-d1)
+#
+#
+# def delta(s, k, sigma, r, t, option_type, position_type):
+#     d1 = (np.log(s / k) + (r + pow(sigma, 2) / 2.0) * t) / (sigma * np.sqrt(t))
+#     if option_type == 'Call':
+#         if position_type == 'long':
+#             return norm.cdf(d1)
+#         else:
+#             return -norm.cdf(d1)
+#     else:
+#         if position_type == 'long':
+#             return norm.cdf(d1) - 1.0
+#         else:
+#             return 1.0 - norm.cdf(d1)
+#
+#
+# def gamma(s, k, sigma, r, t):
+#     d1 = (np.log(s / k) + (r + pow(sigma, 2) / 2.0) * t) / (sigma * np.sqrt(t))
+#     return np.exp(-pow(d1, 2) / 2.0) / (s * sigma * np.sqrt(2.0 * np.pi * t))
+#
+#
+# def theta(s, k, sigma, r, t, option_type):
+#     d1 = (np.log(s / k) + (r + pow(sigma, 2) / 2.0) * t) / (sigma * np.sqrt(t))
+#     d2 = d1 - sigma * np.sqrt(t)
+#     theta_call = -(s * sigma * np.exp(-pow(d1, 2) / 2.0)) / (2.0 * np.sqrt(2.0 * np.pi * t)) - \
+#         r * k * np.exp(-r * t) * norm.cdf(d2)
+#     if option_type == 'Call':
+#         return theta_call
+#     else:
+#         return theta_call + r * k * np.exp(-r * t)
+#
+#
+# def vega(s, k, sigma, r, t):
+#     d1 = (np.log(s / k) + (r + pow(sigma, 2) / 2.0) * t) / (sigma * np.sqrt(t))
+#     return s * np.sqrt(t) * np.exp(-pow(d1, 2) / 2.0) / np.sqrt(2.0 * np.pi)
+#
+#
+# def rho(s, k, sigma, r, t, option_type):
+#     d1 = (np.log(s / k) + (r + pow(sigma, 2) / 2.0) * t) / (sigma * np.sqrt(t))
+#     d2 = d1 - sigma * np.sqrt(t)
+#     if option_type == 'Call':
+#         return k * t * np.exp(-r * t) * norm.cdf(d2)
+#     else:
+#         return -k * t * np.exp(-r * t) * norm.cdf(-d2)
+
+
+def bs_call(s, k, sigma, r, t):
+    tmp = sqrt(t)
+    d1 = (log(s / k) + (r + pow(sigma, 2) / 2.0) * t) / (sigma * tmp)
+    d2 = d1 - sigma * tmp
+    return s * norm.cdf(d1) - k * exp(-r * t) * norm.cdf(d2)
+
+
+def bs_put(s, k, sigma, r, t):
+    tmp = sqrt(t)
+    d1 = (log(s / k) + (r + pow(sigma, 2) / 2.0) * t) / (sigma * tmp)
+    d2 = d1 - sigma * tmp
+    return k * exp(-r * t) * norm.cdf(-d2) - s * norm.cdf(-d1)
+
+
+def call_iv(c, s, k, t, r=0.03, sigma_min=0.01, sigma_max=1.0, e=0.00001):
+    sigma_mid = (sigma_min + sigma_max) / 2.0
+    call_min = bs_call(s, k, sigma_min, r, t)
+    call_max = bs_call(s, k, sigma_max, r, t)
+    call_mid = bs_call(s, k, sigma_mid, r, t)
+    diff = c - call_mid
+    if c <= call_min:
+        return sigma_min
+    elif c >= call_max:
+        return sigma_max
+    while abs(diff) > e:
+        if c > call_mid:
+            sigma_min = sigma_mid
+        else:
+            sigma_max = sigma_mid
+        sigma_mid = (sigma_min + sigma_max) / 2.0
+        call_mid = bs_call(s, k, sigma_mid, r, t)
+        diff = c - call_mid
+    # print(sigma_mid)
+    return sigma_mid
+
+
+def put_iv(c, s, k, t, r=0.03, sigma_min=0.01, sigma_max=1.0, e=0.00001):
+    sigma_mid = (sigma_min + sigma_max) / 2.0
+    put_min = bs_put(s, k, sigma_min, r, t)
+    put_max = bs_put(s, k, sigma_max, r, t)
+    put_mid = bs_put(s, k, sigma_mid, r, t)
+    diff = c - put_mid
+    if c <= put_min:
+        return sigma_min
+    elif c >= put_max:
+        return sigma_max
+    while abs(diff) > e:
+        if c > put_mid:
+            sigma_min = sigma_mid
+        else:
+            sigma_max = sigma_mid
+        sigma_mid = (sigma_min + sigma_max) / 2.0
+        put_mid = bs_put(s, k, sigma_mid, r, t)
+        diff = c - put_mid
+    return sigma_mid
+
+
+def my_test():
+    call_iv(0.138, 3.046, 3.1, 0.5, r=0.03, sigma_min=0.01, sigma_max=1.0, e=0.000001)
+
+
+if __name__ == '__main__':
+    my_test()
+
---
+++ historical_implied_volatility.py
@@ -0,0 +1,161 @@
+"""
+Author: shifulin
+Email: shifulin666@qq.com
+"""
+import math
+import datetime
+from io import BytesIO
+from copy import deepcopy
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sina_stock_kline_api import get_stock_day_kline
+from sina_future_kline_api import get_future_day_kline
+from sina_commodity_option_api import get_option_kline as get_future_option_day_kline
+from sina_etf_option_api import get_option_day_kline as get_etf_option_day_kline
+from european_option import call_iv, put_iv
+
+
+ETF_SPOT_CODE = {
+    '510050': 'sh510050',
+    '510300': 'sh510300',
+}
+
+STOCK_SPOT_CODE = deepcopy(ETF_SPOT_CODE)
+STOCK_SPOT_CODE.update({
+    '159919': 'sz159919',
+    '000300': 'sh000300',
+})
+
+
+def days_interval(date1, date2):
+    d1 = datetime.datetime.strptime(str(date1), "%Y%m%d")
+    d2 = datetime.datetime.strptime(str(date2), "%Y%m%d")
+    days = abs((d1 - d2).days)
+    return days, float(days) / 365.0
+
+
+def get_kline(option_code, spot_code):
+    if spot_code in ETF_SPOT_CODE:
+        option_kline = get_etf_option_day_kline(option_code)
+    else:
+        option_kline = get_future_option_day_kline(option_code)
+    if spot_code in STOCK_SPOT_CODE:
+        spot_kline = get_stock_day_kline(STOCK_SPOT_CODE[spot_code])
+    else:
+        spot_kline = get_future_day_kline(spot_code)
+    return option_kline, spot_kline
+
+
+def cal_hv(y, window_size):
+    len_y = len(y)
+    if len_y >= window_size:
+        factor = math.sqrt(252)
+        hv = [np.std(y[i: i + window_size]) * factor for i in range(len(y) - window_size + 1)]
+        return [np.nan for _ in range(window_size)] + hv
+    else:
+        return [np.nan for _ in range(len_y + 1)]
+
+
+def align_kline(option_kline, spot_kline, window_size):
+    if not option_kline or not spot_kline:
+        return [], []
+    else:
+        if 'c' in option_kline[0]:
+            date_key, close_key, date_func = 'd', 'c', lambda x: int(''.join(x.split('-')))
+        else:
+            date_key, close_key, date_func = 'date', 'close', lambda x: int(''.join(x[:10].split('-')))
+        option_data = [(date_func(i[date_key]), float(i[close_key])) for i in option_kline]
+        if 'c' in spot_kline[0]:
+            date_key, close_key, date_func = 'd', 'c', lambda x: int(''.join(x.split('-')))
+        else:
+            date_key, close_key, date_func = 'date', 'close', lambda x: int(''.join(x[:10].split('-')))
+        tmp_hv, last_close = [], None
+        for i in spot_kline:
+            this_close = float(i[close_key])
+            if last_close is not None:
+                tmp_hv.append(math.log(this_close / last_close))
+            last_close = this_close
+        hv = cal_hv(tmp_hv, window_size)
+        spot_data, spot_hv = [], []
+        len_option_data = len(option_data)
+        index = 0
+        for k, h in zip(spot_kline, hv):
+            k_date = date_func(k[date_key])
+            op_date = option_data[index][0]
+            while k_date > op_date:
+                # print(f'Warning, miss option kline date: {k_date}', option_data[index])
+                del option_data[index]
+                len_option_data -= 1
+                op_date = option_data[index][0]
+            if k_date == op_date:
+                spot_data.append((k_date, float(k[close_key])))
+                spot_hv.append(h)
+                index += 1
+            else:
+                if index >= len_option_data:
+                    break
+        # print(option_data)
+        # print(spot_data)
+        assert len_option_data == len(spot_data)
+        return option_data, spot_data, spot_hv
+
+
+def cal_historical_iv(option_kline, spot_kline, strike_price, expiry_date, r, option_type, exercise_type):
+    if exercise_type == 'european':
+        iv_func = call_iv if option_type == 'call' else put_iv
+    else:
+        pass
+    x, y = [], []
+    for option, spot in zip(option_kline, spot_kline):
+        x.append(str(option[0]))
+        t = days_interval(option[0], expiry_date)[1]
+        y.append(iv_func(option[1], spot[1], strike_price, t, r=r))
+    return x, y
+
+
+def draw_picture(option_code, x, iv, hv, show=True):
+    # print(len(x), len(iv), len(hv))
+    interval = math.ceil(len(x) / 20)
+    x_index = list(range(len(x)))[::-interval]
+    x_label = x[::-interval]
+    fig, axs = plt.subplots(3, sharex=True, gridspec_kw={'hspace': 0})
+    gs = axs[0].get_gridspec()
+    for ax in axs[0: 2]:
+        ax.remove()
+    ax0 = fig.add_subplot(gs[0: 2], sharex=axs[2])
+    ax0.plot(iv)
+    ax0.plot(hv)
+    ax0.set_xlim((0, len(x) - 1))
+    ax0.grid()
+    ax0.set_title(option_code)
+    plt.setp(ax0.get_xticklabels(), visible=False)
+    ax0.legend(['implied volatility', 'historical volatility'])
+    axs[2].axhline(0, color='k', lw=1)
+    axs[2].plot(np.array(hv) - np.array(iv), 'r')
+    axs[2].set_xticks(x_index[::-1])
+    axs[2].set_xticklabels(x_label[::-1], rotation=60)
+    axs[2].set_xlim((0, len(x) - 1))
+    axs[2].set_ylabel('hv - iv')
+    axs[2].grid()
+    fig.tight_layout()
+    if show:
+        plt.show()
+    else:
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png')
+        return buffer.getvalue()
+
+
+def main(option_code, spot_code, strike_price, expiry_date, option_type, exercise_type, window_size):
+    option_kline, spot_kline = get_kline(option_code, spot_code)
+    op_k, sp_k, hv = align_kline(option_kline, spot_kline, window_size=window_size)
+    x, iv = cal_historical_iv(op_k, sp_k, strike_price, expiry_date, 0.03, option_type, exercise_type)
+    draw_picture(option_code, x, iv, hv)
+
+
+if __name__ == '__main__':
+    main('cu2003C51000', 'cu2003', 51000.0, '20200224', 'call', 'european', 5)
+    # main('au2004P340', 'au2004', 340.0, '20200325', 'put', 'european', 10)
+    # main('10002062', '510050', 3.0, '20200122', 'put', 'european', 20)
